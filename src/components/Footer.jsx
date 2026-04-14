7447b28590588c3c4a5e0b6b1ddaeff9947344ea
AlchyRotmg <150161370+AlchyRotmg@users.noreply.github.com>
2026-04-14 08:51:33 -0230

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ function Footer() {
   return (
     <footer className="footer-bar">
       <span>CR2980 Capstone Project - 2026</span>
+      <span>Corbin, Blade, Liam, Nick</span>
     </footer>
   );
 }
